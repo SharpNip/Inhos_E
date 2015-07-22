44fdf1bdc80867a030899285b30e18ff2065fe4d
Sharpnip <1430047@apps.bart.qc.ca>
2015-07-22 16:11:31 -0400

    Made changes yo

--- TP_Final_IE/Assets/Scripts/Maya.cs
+++ TP_Final_IE/Assets/Scripts/Maya.cs
@@ -1,24 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Maya : MonoBehaviour 
+public class Maya 
+    : MonoBehaviour 
 {
     private Rigidbody2D body;
     private BoxCollider2D collider;
-
     private const float MAX_RAD = 100;
     private const float MAX_SPEED = 25;
     private float radLevel;
     private float speed;
-
- //   private const Vector3 LEFT(-1.0, 1, 0);
- //   private const Vector3 RIGHT(1.0, 1, 0);
-
-
+    private bool isIdle;
     private bool isSafe;
     private bool canClimb;
 
-	// Use this for initialization
 	void Start () 
     {
         body = GetComponent<Rigidbody2D>();
@@ -26,73 +21,91 @@ public class Maya : MonoBehaviour
         radLevel = 0;
         speed = 0;
         isSafe = false;
+        canClimb = false;
+        isIdle = true;
+        
 	}
 
     void FixedUpdate()
     {
+        float dt = Time.deltaTime;
         if (!isSafe)
         {
-            radLevel++;
+            Irradiate();
         }
-        if (canClimb)
+        else
         {
-            if (Input.GetKey("w"))
+            Heal();
+        }
+        if (Input.GetKey("a"))
+        {
+            WalkLeft(dt);
+        }
+        else if (Input.GetKey("d"))
+        {
+            WalkRight(dt);
+        }
+        else if (Input.GetKey("w"))
+        {
+            if (canClimb)
             {
                 Climb();
             }
         }
-
-        if (Input.GetKey("a"))
+        else
         {
-            //Walk(LEFT);
+            isIdle = true;
         }
-        if (Input.GetKey("d"))
-        {
-            //Walk(RIGHT);
-        }
-
-        
 
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.name == "Safe")
+        if (other.CompareTag("Safe"))
         {
             isSafe = true;
         }
-        if (other.name == "Door")
+        if (other.CompareTag("Ladder"))
         {
             canClimb = true;
         }
-        if (other.name == "Bottle")
+        if (other.CompareTag("Bottle"))
         {
             radLevel = 0;
         }
     }
+
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.name == "Safe")
+        if (other.CompareTag("Safe"))
         {
             isSafe = false;
         }
     }
 
-
     private void Jump()
     {
-        //body.AddForce();
     }
-    private void Walk(Vector3 direction)
+
+    private void WalkLeft(float deltaTime)
     {
-        //body.
     }
+
+    private void WalkRight(float deltaTime)
+    {
+    }
+
     private void Climb()
     {
-       
     }
 
+    private void Irradiate()
+    {
+        
+    }
 
-	
+    private void Heal()
+    {
 
+    }
 }
