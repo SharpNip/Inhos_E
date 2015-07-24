85f93a1bc739d714b9967d1496c5af69b11204a2
Nick <1430047@apps.bart.qc.ca>
2015-07-23 22:23:27 -0400

    Somewhat functional movement

--- TP_Final_IE/Assets/Scripts/Maya.cs
+++ TP_Final_IE/Assets/Scripts/Maya.cs
@@ -4,72 +4,125 @@ using System.Collections;
 public class Maya 
     : MonoBehaviour 
 {
-    private Rigidbody2D body;
-    private BoxCollider2D collider;
-    private const float MAX_RAD = 100;
-    private const float MAX_SPEED = 25;
-    private float radLevel;
-    private float speed;
-    private bool isIdle;
-    private bool isSafe;
-    private bool canClimb;
+	// Status Variables
+	private bool isIdle;
+	private bool isSafe;
+	private bool canClimb;
+	private bool onGround;
+	private float radLevel;
+
+	// Movement variables
+	private const float MAX_RAD = 100;
+	private const float MAX_SPD = 3;
+	private const float MAX_CLIMBSPD = 2;
+	private Vector2 direction;
+	private float jumpHeight;
+	private float speed;
+	private float climbSpeed;
+
+	// Body components
+	private Rigidbody2D body;
+	private BoxCollider2D collider;
+
+	// Collider Tags
+	private const string GROUND = "Ground";
+	private const string SAFE_AREA = "Safe";
+	private const string SPIKES = "Spikes";
+	private const string DANGER_AREA = "RadBarrel";
+	private const string LADDER = "Ladder";
+	private const string SDPTA = "Bottle";
 
 	void Start () 
     {
+		//Initialiasation of body components
         body = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
-        radLevel = 0;
+        //Setting all status variables
+		radLevel = 0;
         speed = 0;
+		climbSpeed = 0;
+		jumpHeight = 3;
         isSafe = false;
         canClimb = false;
         isIdle = true;
-        
+		onGround = true;
 	}
 
-    void FixedUpdate()
-    {
-        float dt = Time.deltaTime;
-        if (!isSafe)
-        {
-            Irradiate();
-        }
-        else
-        {
-            Heal();
-        }
-        if (Input.GetKey("a"))
-        {
-            WalkLeft(dt);
-        }
-        else if (Input.GetKey("d"))
-        {
-            WalkRight(dt);
-        }
-        else if (Input.GetKey("w"))
-        {
-            if (canClimb)
-            {
-                Climb();
-            }
-        }
-        else
-        {
-            isIdle = true;
-        }
+	void Update()
+	{
+		HandleStates();
+		ManageInput();    
+	}
 
-    }
+	private void HandleStates()
+	{
+		if (!isSafe)
+		{
+			Irradiate();
+		}
+		else
+		{
+			Heal();
+		}
+	}
+
+	private void ManageInput()
+	{
+		if (Input.GetKey("a"))
+		{
+			direction = -Vector2.right;
+			Walk();
+		}
+		else if (Input.GetKey("d"))
+		{
+			direction = Vector2.right;
+			Walk();
+		}
+		else if (Input.GetKey("w"))
+		{
+			if(canClimb)
+			{
+				Climb();
+			}
+			else
+			{
+				Jump();
+			}
+		}
+		else
+		{
+			isIdle = true;
+		}
+	}
+    
+	void OnCollisionStay2D(Collision2D collider)
+	{
+		// Checks for ground contact
+		onGround = (collider.gameObject.CompareTag (GROUND)) ? true : false;
+	}
+	void OnCollisionEnter2D(Collision2D collider)
+	{
+		if (collider.gameObject.CompareTag (SPIKES)) 
+		{
+			Debug.Log("You're Dead!!");
+		}
+	}
 
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("Safe"))
+        if (other.CompareTag(SAFE_AREA))
         {
             isSafe = true;
-        }
-        if (other.CompareTag("Ladder"))
+		}
+		if (other.CompareTag (DANGER_AREA)) 
+		{
+
+		}
+        if (other.CompareTag(LADDER))
         {
             canClimb = true;
         }
-        if (other.CompareTag("Bottle"))
+        if (other.CompareTag(SDPTA))
         {
             radLevel = 0;
         }
@@ -77,7 +130,7 @@ public class Maya
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Safe"))
+        if (other.CompareTag(SAFE_AREA))
         {
             isSafe = false;
         }
@@ -85,18 +138,27 @@ public class Maya
 
     private void Jump()
     {
+		if(onGround)
+		{
+			body.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+			onGround = false;
+		}
     }
 
-    private void WalkLeft(float deltaTime)
+    private void Walk()
     {
-    }
-
-    private void WalkRight(float deltaTime)
-    {
+        if (speed < MAX_SPD)
+        {
+            speed += MAX_SPD * Time.deltaTime;
+        }
+        body.velocity = direction * speed;
     }
 
     private void Climb()
     {
+		if(speed < MAX_CLIMBSPD)
+
+		body.velocity = direction * speed;
     }
 
     private void Irradiate()
